b41128d5278d3d9219572784c104be16cd094656
Khodile <gnevushev-98@mail.ru>
2024-04-04 21:02:50 +0300

    Add configuration via config file, edit default values

--- README.md
+++ README.md
@@ -12,22 +12,26 @@ You must have installed ffmpeg, ffprobe and [yt-dlp](https://github.com/yt-dlp/y
 ```git clone https://github.com/Khodile/coub-dlp-script.git```
 
 2. Configure the script
-   a) Open coub-dlp.sh in your favourite text editor.
-   b) Change the parameter values to the desired ones on lines 3 to 12.
-      (Note: Strings for the *_DIRECTORY parameters must end with /)
+   * Create configuration file ~/.config/coub-dlp and write parameters one by line (Example: MINIMAL_DURATION_SECONDS=45)
+   OR
+   * Configuration file with default values will be created at first start (with all default folders and files)
 
 3. Create batch-file and archive-file in choosen destinations
 
 4. Copy urls to the batch-file
 
-5. Start script
+5. Add execute permission
+
+```chmod +x coub-dlp.sh```
+
+6. Start script
 
 ```bash /path/to/script/coub-dlp.sh```
 
 ## Parameters
 | Parameter                | Type   | Description |
 |--------------------------|--------|-------------|
-| MINIMAL_DURATION_SECONDS | int    | Target minimal duration of resulting video files in seconds
+| MINIMAL_DURATION_SECONDS | int    | Target minimal duration of resulting video files in seconds |
 | FREE_SPACE_RESERVE_KiB   | int    | Minimal amount of free space in target directory in KiB at which processing can be started |
 | YT_DLP_ARCHIVE_FILE      | String | Path to the download-archive file for yt-dlp call. Yt-dlp download only videos not listed in the archive file. Record the IDs of all downloaded videos in it |
 | YT_DLP_BATCH_FILE        | String | Path to the batch-file for yt-dlp call. Batch-file is a file containing URLs to download ("-" for stdin), one URL per line. Lines starting with "#", ";" or "]" are considered as comments and ignored |
@@ -35,3 +39,15 @@ You must have installed ffmpeg, ffprobe and [yt-dlp](https://github.com/yt-dlp/y
 | YT_DLP_TARGET_DIRECTORY  | String | Path to the directory where files will be saved by yt-dlp |
 | YT_DLP_OUTPUT_TEMPLATE   | String | Yt-dlp [output template](https://github.com/yt-dlp/yt-dlp#output-template) (path relative to YT_DLP_TARGET_DIRECTORY).  |
 | FINAL_TARGET_DIRECTORY   | String | Path to the directory where resulting video files will be saved |
+
+## Default values
+| Parameter | Value |
+|-----------|-------|
+| MINIMAL_DURATION_SECONDS | 45 |
+| FREE_SPACE_RESERVE_KiB   | 102400 |
+| YT_DLP_ARCHIVE_FILE      | '~/.coub-dlp/yt-dlp/downloaded.txt' |
+| YT_DLP_BATCH_FILE        | '~/.coub-dlp/yt-dlp/batch-file.txt' |
+| LOGS_DIRECTORY           | '~/.coub-dlp/logs/' |
+| YT_DLP_TARGET_DIRECTORY  | '~/.coub-dlp/coubs/temp/' |
+| YT_DLP_OUTPUT_TEMPLATE   | '%(title)s by <%(uploader)s> [%(id)s].%(ext)s' |
+| FINAL_TARGET_DIRECTORY   | '~/.coub-dlp/coubs/final/' |
--- coub-dlp.sh
+++ coub-dlp.sh
@@ -1,20 +1,76 @@
 #!/bin/bash
 
+# Default variables
 MINIMAL_DURATION_SECONDS=45
-FREE_SPACE_RESERVE_KiB=1024
+FREE_SPACE_RESERVE_KiB=102400
 
-YT_DLP_ARCHIVE_FILE='./yt-dlp/downloaded.txt'
-YT_DLP_BATCH_FILE='./yt-dlp/batch-file.txt'
-LOGS_DIRECTORY='./logs/'
+YT_DLP_ARCHIVE_FILE='~/.coub-dlp/yt-dlp/downloaded.txt'
+YT_DLP_BATCH_FILE='~/.coub-dlp/yt-dlp/batch-file.txt'
+LOGS_DIRECTORY='~/.coub-dlp/logs/'
 
-YT_DLP_TARGET_DIRECTORY='./coubs/temp/'
+YT_DLP_TARGET_DIRECTORY='~/.coub-dlp/coubs/temp/'
 YT_DLP_OUTPUT_TEMPLATE='%(title)s by <%(uploader)s> [%(id)s].%(ext)s'
-FINAL_TARGET_DIRECTORY='./coubs/final/'
+FINAL_TARGET_DIRECTORY='~/.coub-dlp/coubs/final/'
+
+# Create config file with default variables
+if [[ ! -e "~/.config/coub-dlp" ]]
+then
+  echo "MINIMAL_DURATION_SECONDS=45" > ~/.config/coub-dlp
+  echo "FREE_SPACE_RESERVE_KiB=102400" >> ~/.config/coub-dlp
+  echo "YT_DLP_ARCHIVE_FILE='~/.coub-dlp/yt-dlp/downloaded.txt'" >> ~/.config/coub-dlp
+  echo "YT_DLP_BATCH_FILE='~/.coub-dlp/yt-dlp/batch-file.txt'" >> ~/.config/coub-dlp
+  echo "LOGS_DIRECTORY='~/.coub-dlp/logs/'" >> ~/.config/coub-dlp
+  echo "YT_DLP_TARGET_DIRECTORY='~/.coub-dlp/coubs/temp/'" >> ~/.config/coub-dlp
+  echo "YT_DLP_OUTPUT_TEMPLATE='%(title)s by <%(uploader)s> [%(id)s].%(ext)s'" >> ~/.config/coub-dlp
+  echo "FINAL_TARGET_DIRECTORY='~/.coub-dlp/coubs/final/'" >> ~/.config/coub-dlp
+fi
+
+# Export variables from config file
+set -a
+. ~/.config/coub-dlp
+set +a
 
 
+# Create directories and files
+if [[ ! -d "$LOGS_DIRECTORY" ]]
+then
+  mkdir -p "$LOGS_DIRECTORY"
+fi
+
+if [[ ! -d "$YT_DLP_TARGET_DIRECTORY" ]]
+then
+  mkdir -p "$YT_DLP_TARGET_DIRECTORY"
+fi
+
+if [[ ! -d "$FINAL_TARGET_DIRECTORY" ]]
+then
+  mkdir -p "$FINAL_TARGET_DIRECTORY"
+fi
+
+if [[ ! -e "$YT_DLP_ARCHIVE_FILE" ]]
+then
+  if [[ ! -d "${YT_DLP_ARCHIVE_FILE%/*}" ]]
+  then
+    mkdir -p "${YT_DLP_ARCHIVE_FILE%/*}"
+  fi
+  touch $YT_DLP_ARCHIVE_FILE
+fi
+
+if [[ ! -e "$YT_DLP_BATCH_FILE" ]]
+then
+  if [[ ! -d "${YT_DLP_BATCH_FILE%/*}" ]]
+  then
+    mkdir -p "${YT_DLP_BATCH_FILE%/*}"
+  fi
+  touch $YT_DLP_BATCH_FILE
+fi
+
+
+# Log filename template
 TIMESTAMP="$(date +'%Y-%m-%d_%H:%M:%S')"
 LOG_FILE="${LOGS_DIRECTORY}${TIMESTAMP}_coub-dlp.log"
 
+# Download video and audio
 yt-dlp --ignore-config --download-archive $YT_DLP_ARCHIVE_FILE --batch-file $YT_DLP_BATCH_FILE --format bestvideo/mp4/bestvideo*,bestaudio/mp3/bestaudio* --output "${YT_DLP_TARGET_DIRECTORY}${YT_DLP_OUTPUT_TEMPLATE}"
 
 echo -e "All coubs are downloaded.\n" > $LOG_FILE
@@ -32,7 +88,7 @@ do
   if [[ -e "$AUDIO_FILE" && -e "$VIDEO_FILE" && $FREE_SPACE -gt $FREE_SPACE_RESERVE_KiB ]]
   then
     echo -e "\t\tThere is enough free space. $FREE_SPACE / $FREE_SPACE_RESERVE_KiB\n" >> $LOG_FILE
-    echo -e "\t\tFound audio file:\n\t$AUDIO_FILE\n" >> $LOG_FILE
+    echo -e "\t\tFound audio file:\n\t\t\t$AUDIO_FILE\n" >> $LOG_FILE
     
     LOOPED_VIDEO_FILE="${VIDEO_FILE%.mp4}_looped.mp4"
     LOOPED_VIDEO_FILE="${LOOPED_VIDEO_FILE/$YT_DLP_TARGET_DIRECTORY/$FINAL_TARGET_DIRECTORY}"
@@ -45,21 +101,29 @@ do
     then
       echo -e "\t\tDuration of the audio file is not enough. $AUDIO_DURATION < $MINIMAL_DURATION_SECONDS\n" >> $LOG_FILE
       
-      LOOPS_COUNT=$(echo -e $MINIMAL_DURATION_SECONDS $AUDIO_DURATION | awk '{printf("%d",$1 / $2 + 1)}')
-      ffmpeg -y -stream_loop "$LOOPS_COUNT" -i "$AUDIO_FILE" -c copy "$LOOPED_AUDIO_FILE"
-      AUDIO_DURATION=$(ffprobe -i "$LOOPED_AUDIO_FILE" -show_entries format=duration -v quiet -of csv="p=0")
+      LOOPS_COUNT=$(echo -e $MINIMAL_DURATION_SECONDS $AUDIO_DURATION | awk '{printf($1 / $2)}')
+      if [[ $(echo -e "$LOOPS_COUNT > $(echo -e $LOOPS_COUNT | awk '{printf("%d",$1)}')" | bc) -eq 1 ]]
+      then
+        LOOPS_COUNT=$(echo -e $LOOPS_COUNT | awk '{printf("%d",$1 + 1)}')
+      fi
     else
       echo -e "\t\tDuration of the audio file is enough. $AUDIO_DURATION >= $MINIMAL_DURATION_SECONDS\n" >> $LOG_FILE
       
-      ffmpeg -y -stream_loop 0 -i "$AUDIO_FILE" -c copy "$LOOPED_AUDIO_FILE"
+      LOOPS_COUNT=0
     fi
+    ffmpeg -y -stream_loop "$LOOPS_COUNT" -i "$AUDIO_FILE" -c copy "$LOOPED_AUDIO_FILE"
+    AUDIO_DURATION=$(ffprobe -i "$LOOPED_AUDIO_FILE" -show_entries format=duration -v quiet -of csv="p=0")
     VIDEO_DURATION=$(ffprobe -i "$VIDEO_FILE" -show_entries format=duration -v quiet -of csv="p=0")
     
     if [[ $(echo -e "$VIDEO_DURATION < $AUDIO_DURATION" | bc) -eq 1 ]]
     then
       echo -e "\t\tDuration of the video file is not enough. $VIDEO_DURATION < $AUDIO_DURATION\n" >> $LOG_FILE
       
-      LOOPS_COUNT=$(echo -e $AUDIO_DURATION $VIDEO_DURATION | awk '{printf("%d",$1 / $2 + 1)}')
+      LOOPS_COUNT=$(echo -e $AUDIO_DURATION $VIDEO_DURATION | awk '{printf($1 / $2)}')
+      if [[ $(echo -e "$LOOPS_COUNT > $(echo -e $LOOPS_COUNT | awk '{printf("%d",$1)}')" | bc) -eq 1 ]]
+      then
+        LOOPS_COUNT=$(echo -e $LOOPS_COUNT | awk '{printf("%d",$1 + 1)}')
+      fi
     else
       echo -e "\t\tDuration of the video file is enough. $VIDEO_DURATION >= $AUDIO_DURATION\n" >> $LOG_FILE
       
@@ -73,6 +137,7 @@ do
     ffmpeg -y -i "$LOOPED_VIDEO_FILE" -i "$LOOPED_AUDIO_FILE" -shortest "$RESULT_FILE"
     
     
+    # Remove audio, video, looped audio and looped video files
     if [[ -e "$RESULT_FILE" && "$RESULT_FILE" != "$AUDIO_FILE" ]]
     then
       echo -e "\t\tRemoving initial audio file.\n" >> $LOG_FILE
@@ -97,10 +162,10 @@ do
       rm "$LOOPED_VIDEO_FILE"
     fi
     
-    echo -e "\tProcessing ended successfully." >> $LOG_FILE
+    echo -e "\tProcessing ended successfully.\n" >> $LOG_FILE
     
   else
-    echo -e "\tThere is not enough free space ($FREE_SPACE / $FREE_SPACE_RESERVE_KiB) or file(s) not exist(s)." >> $LOG_FILE
+    echo -e "\tThere is not enough free space ($FREE_SPACE / $FREE_SPACE_RESERVE_KiB) or file(s) not exist(s).\n" >> $LOG_FILE
   fi
 done
 
